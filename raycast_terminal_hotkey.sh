1258bd528465e254f0e69e41e029c7af882c2dd8
Paranoid Developer <du.brisingr.arget@gmail.com>
2024-03-19 14:34:03 +0900

    added raycast script that opens terminal on hotkey

---
+++ raycast_terminal_hotkey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Terminal
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName macos_scripts
+
+# Documentation:
+# @raycast.description opens terminal
+# @raycast.author Paranoid-Dev
+# @raycast.authorURL https://github.com/Paranoid-Dev
+
+open -a Terminal --new
